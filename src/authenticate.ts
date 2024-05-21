9891b0b09fc2b16d8e5ba0642f6461bc5c0475c1
Mark Furland <mark@fur.land>
2024-05-21 16:46:00 -0400

    at least it works I guess

--- src/authenticate.ts
+++ src/authenticate.ts
@@ -1,63 +1,80 @@
 import {
     APIGatewayRequestAuthorizerEvent,
-    APIGatewaySimpleAuthorizerResult,
-    APIGatewaySimpleAuthorizerWithContextResult,
+    APIGatewayAuthorizerResult,
+    APIGatewayAuthorizerWithContextResult,
+    APIGatewayAuthorizerResultContext,
+APIGatewayRequestAuthorizerEventHeaders,
 } from "aws-lambda";
 
-export interface AuthorizationContext {
+export interface AuthorizationContext extends APIGatewayAuthorizerResultContext {
     read: boolean;
     write: boolean;
+};
+
+
+// decode base64
+// https://stackoverflow.com/a/61155795/2423187
+const decodeb64 = (str: string): string =>
+    Buffer.from(str, "base64").toString("binary");
+
+export const basicAuth = (authHeader: string): AuthorizationContext => {
+    const matches = authHeader.match(/Basic (.*)$/i);
+    if (!matches) {
+        throw new Error("Authorization header wasn't Basic or something");
+    }
+
+    const userPass = decodeb64(matches[1]!);
+    if (userPass == "foo:bar") {
+        return {
+            read: true,
+            write: true,
+        };
+    } else {
+        throw new Error("invalid username/pass");
+    }
+    throw new Error("shouldn't be here");
+};
+
+const getParameterCaseInsensitive = (o: APIGatewayRequestAuthorizerEventHeaders | null, key: string): string | undefined => {
+    if(!o) {
+        return undefined;
+    }
+    const asLowercase = key.toLowerCase();
+    const k = Object.keys(o).find(k => k.toLowerCase() === asLowercase)
+    return k ? o[k] : undefined;
 }
 
 // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/aws-lambda/trigger/api-gateway-authorizer.d.ts
 export const handler = async (
     event: APIGatewayRequestAuthorizerEvent,
 ): Promise<
-    | APIGatewaySimpleAuthorizerResult
-    | APIGatewaySimpleAuthorizerWithContextResult<AuthorizationContext>
+    | APIGatewayAuthorizerResult
+    | APIGatewayAuthorizerWithContextResult<AuthorizationContext>
 > => {
-    console.log(event.headers);
-    const ret = {
+    const ret: APIGatewayAuthorizerResult | APIGatewayAuthorizerWithContextResult<AuthorizationContext>
+        = {
         "principalId": "I dunno",
-            "policyDocument": {
-                "Version": "2012-10-17",
-                    "Statement": [
-                        {
-                            "Action": "execute-api:Invoke",
-                            "Effect": "Allow",
-                            "Resource": event.methodArn
-                        }
-                    ]
+        "policyDocument": {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                "Action": "execute-api:Invoke",
+                "Effect": "Deny",
+                "Resource": event.methodArn
             }
+            ]
+        }
     }
-    return ret;
 
     try {
-        const authHeader = event.headers?.["authorization"];
+        const authHeader = getParameterCaseInsensitive(event.headers, "authorization");
         if (authHeader) {
-            const matches = authHeader.match(/Basic (.*)$/i);
-            if (!matches) {
-                throw new Error("Authorization header wasn't Basic or something");
-            }
-
-            // TODO: rewrite using buffer for less try/catch nonsense: https://stackoverflow.com/a/61155795/2423187
-            try {
-                const userPass = atob(matches[1]!);
-                if (userPass == "foo:bar") {
-                    return {
-                        isAuthorized: true,
-                        context: {
-                            read: true,
-                            write: true,
-                        },
-                    };
-                } else {
-                    throw new Error("invalid username/pass");
-                }
-            } catch (e) {
-                throw new Error("Couldn't decoee basic auth base64");
-            }
-            throw new Error("shouldn't be here");
+            const context = basicAuth(authHeader);
+            ret.policyDocument.Statement[0].Effect = "Allow";
+            ret.context = {
+                read: true,
+                write: true,
+            };
         }
     } catch (e) {
         if (!e || !(e instanceof Error)) {
@@ -66,7 +83,5 @@ export const handler = async (
         console.error(`auth failed: ${e.message}`);
     }
     // TODO: if we return false, can we have a sane error message?
-    return {
-        isAuthorized: false,
-    };
+    return ret;
 };
